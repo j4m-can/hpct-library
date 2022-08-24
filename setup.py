7a025dfcd9da8ada417b6e47ca9705d91efa2fdb
NucciTheBoss <jason.nucciarone@canonical.com>
2022-08-24 10:50:08 -0400

    Add setup.py to install hpctlib

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# setup.py
+
+"""Install hpctlib with pip."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hpctlib",
+    version="0.1.0",
+    description="Canonical HPC Team's internal library",
+    license="Apache-2.0",
+    packages=find_packages(
+        where="lib",
+        include=["hpctlib*"],
+    ),
+    package_dir={"": "lib"},
+    install_requires=[
+        "cryptography",
+        "ops"
+    ],
+    classifiers=[
+        "Development State :: 1 - Planning",
+        "License :: OSI Approved :: Apache-2.0",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 3"
+    ],
+)
